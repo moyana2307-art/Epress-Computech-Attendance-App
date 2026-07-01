43981c60a840ba6fd01389f02ed614aebc81ae99
Pride Moyana <moyana2307@gmail.com>
2026-07-01 20:45:37 +0200

    Simplify api/index.js

--- epress-attendance/api/index.js
+++ epress-attendance/api/index.js
@@ -1,11 +1,2 @@
-let app;
-try {
-  app = (await import('../server/index.js')).default;
-} catch (err) {
-  console.error('Failed to load app:', err);
-  app = (req, res) => {
-    res.status(500).json({ error: err.message, stack: err.stack });
-  };
-}
-
+import app from '../server/index.js';
 export default app;
