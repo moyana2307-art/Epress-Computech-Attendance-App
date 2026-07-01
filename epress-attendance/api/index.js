1ca699ae64ad2febcf98bc1fd9c537ece205c219
Pride Moyana <moyana2307@gmail.com>
2026-07-01 20:36:42 +0200

    Debug api handler

--- epress-attendance/api/index.js
+++ epress-attendance/api/index.js
@@ -1,3 +1,11 @@
-import app from '../server/index.js';
+let app;
+try {
+  app = (await import('../server/index.js')).default;
+} catch (err) {
+  console.error('Failed to load app:', err);
+  app = (req, res) => {
+    res.status(500).json({ error: err.message, stack: err.stack });
+  };
+}
 
 export default app;
